ff83159fa1300f29e3e061dd874213cc5026b503
leona_000 <leona_000@Evolve>
2016-09-25 18:18:17 -0500

    Version Final

---
+++ ConversorMain/src/conversormain/ConversorMain.java
@@ -0,0 +1,11 @@
+
+package conversormain;
+
+public class ConversorMain {
+
+    public static void main(String[] args) {
+        Conversor co = new Conversor();
+        co.setVisible(true);
+    }
+    
+}
